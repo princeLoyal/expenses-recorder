e09556e27c9eb92dec6a5aee162d6be5889d305b
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-12 20:21:00 +0100

    Update CreateAccount.js

--- src/components/UI/CreateAccount.js
+++ src/components/UI/CreateAccount.js
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from 'react';
 
 const CreateAccount = (props) => {
-  
+  const emailRef = useRef();
 
   return(
     <form>
-       <input type='email' />
+       <input type='email' ref={emailRef}/>
        <input type='password' />
        <button type='submit'>Login</button>
     </form>
