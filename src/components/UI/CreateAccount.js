dd213f4fb527ae58c096c387931eab69abdef455
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-12 21:09:34 +0100

    Update CreateAccount.js

--- src/components/UI/CreateAccount.js
+++ src/components/UI/CreateAccount.js
@@ -41,7 +41,7 @@ const CreateAccount = (props) => {
        <input id='password' type='password' ref={passwordRef}/>
        <div className='form-actions'>
          <button type='submit'>Create Account</button>
-         <button type='button' onClick={() => props.loginButtonClick('createAccount')}>Log In</button>
+         <button type='button' onClick={() => props.onloginButtonClick('createAccount')}>Log In</button>
        </div>
     </form>
   );
