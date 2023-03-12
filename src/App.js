ea48446e85fb3e540ccb9f8218d9de43512b577e
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-12 14:41:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,37 +21,11 @@ const expenses = [
   ];
 
 function App() {
-  const [logginIn, setLogginIn] = useState(true);
-  const [creatingAccount, setCreatingAccount] = useState(true);
-  const [userEmail, setUserEmail] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [expensesList, setExpensesList] = useState(expenses);
-
-  const onAddExpenses = (expenseData) => {
-    setExpensesList(prevExpensesList => {
-       const updatedExpensesList = [expenseData, ...prevExpensesList];
-       return updatedExpensesList;
-    });
-  }
-  const loginHandler = (email) => {
-     setUserEmail(email);
-     setIsLoggedIn(true);
-  }
-
-  const loginAndCreateAccountSwitchingHandler = (state) => {
-    if(state === 'login'){
-      setLogginIn(false);
-      setCreatingAccount(true);
-    }
-    if(state === 'createAccount'){
-      setLogginIn(true);
-      setCreatingAccount(false);
-    }
-  }
+  
   return (
     <div className="App">
     // { creatingAccount && <CreateAccount onLogin={loginHandler} onClickLogin = {loginAndCreateAccountSwitchingHandler}/> }
-     <Expenses items={expensesList} onAddExpenses={onAddExpenses}/>
+     <Expenses items={expensesList}/>
     </div>
   );
 }
