abdc5ee696082fcdb243f378f94fb49a1bf94a31
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-12 20:44:42 +0100

    Update CreateAccount.js

--- src/components/UI/CreateAccount.js
+++ src/components/UI/CreateAccount.js
@@ -2,12 +2,46 @@ import React, { useRef, useState } from 'react';
 
 const CreateAccount = (props) => {
   const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const formSubmitHander = async (event) = {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = emailRef.current.value;
+    const userData = {
+      email:email,
+      password:password,
+      id: Math.round(Math.random() * 10000),
+    }
+
+    const response = await fetch('https://expenses-recorder-f372f-default-rtdb.firebaseio.com/users.json')
+    const usersList = await response.json();
+    for(const key in usersList){
+      if(usersList[key].email === email){
+         alert('Email already in database. Try logging in.');
+      }
+    }
+
+    const createAccResponse = await fetch('https://expenses-recorder-f372f-default-rtdb.firebaseio.com/users.json',{
+       method: 'POST',
+       body: JSON.stringify(userData),
+     })
+    const data = await createAccResponse.json();
+
+    props.onLogin(email);
+  }
 
   return(
     <form>
-       <input type='email' ref={emailRef}/>
-       <input type='password' />
-       <button type='submit'>Login</button>
+       <label htmlFor='email'>Email: </label>
+       <input id='email' type='email' ref={emailRef}/>
+       <label htmlFor='password'>Password: </label>
+       <input id='password' type='password' ref={passwordRef}/>
+       <div className='form-actions'>
+         <button type='submit'>Create Account</button>
+         <button type='button'>Log In</button>
+       </div>
     </form>
   );
 }
